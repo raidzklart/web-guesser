556b33ae8122cda09359dd6e9f26cf54b62a5a76
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-09-19 21:05:32 +0100

    move guesser class into own file

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,39 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
-
-class Guesser
-  attr_accessor :number, :guesses
-  def initialize
-    setup
-  end
-
-  def check_guess(guess)
-    correct = false
-    if guess > @number
-      guess - 5 > @number ? message = "Way too high!" : message = "Too high!"
-    elsif guess < @number
-      guess + 5 < @number ? message = "Way too low!" : message = "Too low!"
-    elsif guess == @number
-      message = "You got it right! The SECRET NUMBER was #{@number}"
-      setup
-    end
-    guess()
-    message
-  end
-
-  private
-
-  def guess
-    if @guesses == 1
-      setup
-    end
-    @guesses -= 1
-  end
-  def setup
-    @guesses = 6
-    @number = rand(100)
-  end
-end
+require_relative 'guesser.rb'
 
 guesser = Guesser.new
 
