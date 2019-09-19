b6bfb6d0a90882cce868643daada3bc7cb31381f
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-09-19 20:52:54 +0100

    Limit guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,21 +1,45 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+class Guesser
+  attr_accessor :number, :guesses
+  def initialize
+    setup
+  end
+
+  def check_guess(guess)
+    correct = false
+    if guess > @number
+      guess - 5 > @number ? message = "Way too high!" : message = "Too high!"
+    elsif guess < @number
+      guess + 5 < @number ? message = "Way too low!" : message = "Too low!"
+    elsif guess == @number
+      message = "You got it right! The SECRET NUMBER was #{@number}"
+      setup
+    end
+    guess()
+    message
+  end
+
+  private
+
+  def guess
+    if @guesses == 1
+      setup
+    end
+    @guesses -= 1
+  end
+  def setup
+    @guesses = 6
+    @number = rand(100)
+  end
+end
+
+guesser = Guesser.new
+
 get '/' do
   guess = params['guess'].to_i
   cheat = params['cheat']
-  message = check_guess(guess)
-  erb :index, :locals => { :number => SECRET_NUMBER, :message => message, :cheat => cheat }
-end
-
-def check_guess(guess)
-  if guess > SECRET_NUMBER
-    guess - 5 > SECRET_NUMBER ? message = "Way too high!" : message = "Too high!"
-  elsif guess < SECRET_NUMBER
-    guess + 5 < SECRET_NUMBER ? message = "Way too low!" : message = "Too low!"
-  elsif guess == SECRET_NUMBER
-    message = "You got it right!"
-  end
-  message
+  message = guesser.check_guess(guess)
+  erb :index, :locals => { :number => guesser.number, :message => message, :cheat => cheat, :guesses => guesser.guesses }
 end
