87e687c2184da6c785342a83675fd8cd39eaf087
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-09-19 16:19:10 +0100

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,7 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
+number = rand(101)
 get '/' do
-  "Hello, World!"
-end
-
-get '/:name' do |n|
-  "Hello #{n}!"
+  "The secret number is #{number}!"
 end
